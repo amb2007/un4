b67a6977fe8efd06fb3f3f1bdb6ccd0b8f9b6dde
amb2007 <agustinmancinibobadilla@gmail.com>
2024-06-28 12:35:45 -0300

    termine-el-ejercicio-5

--- ej5.py
+++ ej5.py
@@ -1,11 +1,17 @@
 from PIL import Image
-print('ingrese la ruta de la imagen a pegar')
-namew = input()
+namew = input('ingrese la ruta de la imagen a pegar \n')
 wimage = Image.open(namew)
-maxw = wimage.size
-print('ingrese la ruta de la imagen en donde sera pegada la anterior')
-name = input()
+name = input('ingrese la ruta de la imagen en donde sera pegada la anterior \n')
 image = Image.open(name)
-max = image.size
-if max [0]< maxw[0] or max[1] < maxw[1]:
-    wimage.resize((max[0]/2, max[1]/2))
+print(image.size)
+wimage.resize((30, 30))
+posicion = int(input('elija una posicion arriba a la izquiera(1), arriba a la derecha(2), abajo a la izquierda(3), abajo a la derecha(4) \n'))
+if posicion == 1:
+    image.paste(wimage, (0,0))
+elif posicion == 2:
+    image.paste(wimage, (image.size[0]-wimage.size[0],0))
+elif posicion == 3:
+    image.paste(wimage, (0,image.size[1]-wimage.size[1]))
+elif posicion == 4:
+    image.paste (wimage,(image.size[0]-wimage.size[0],image.size[1]-wimage.size[1]))
+image.show()
